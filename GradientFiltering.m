ec7827ae75261ff1608c3a36c39dec4a88a8ce47
Walter2609 <32808292+Walter2609@users.noreply.github.com>
2017-10-15 07:53:19 +0200

    Grad_X = Grad_Y^T

--- GradientFiltering.m
+++ GradientFiltering.m
@@ -2,33 +2,38 @@
 clear all; close all; clc;
 %%
 I = imread('coins.png');
-imshow(I);
-gauss = fspecial('gaussian',5,2);
-I = imfilter(I,gauss);
-I = im2double(I);
 figure(1);
 imshow(I);
 title('Original Picture');
 
 
+%% Filtered Image => Gaussfilter = Bluered Image
+gauss = fspecial('gaussian',5,2);
+I = imfilter(I,gauss);
+I = im2double(I);
+figure(2);
+imshow(I);
+title('Filtered with Gaussian => Bluered Image');
+
+
 %% Gradient in Y-Direction
 prewitt_Y = fspecial('prewitt');
 Grad_Y = imfilter(I,prewitt_Y);
-figure(2);
+figure(3);
 imshow(Grad_Y, []);
 title('Prewitt-Filter: Grad. Y-Direction');
 
 
 %% Gradient in X-Direction
-prewitt_X = prewitt_Y;
-Grad_X = imfilter(I,prewitt_X);    % Transpose Prewitt
-figure(3);
+prewitt_X = prewitt_Y';             % Transpose Prewitt
+Grad_X = imfilter(I,prewitt_X);    
+figure(4);
 imshow(Grad_X, []);
 title('Transpose(Prewitt): Grad. X-Direction');
 
 
 %% Gradient in X- and Y-Direction
-figure(4);
+figure(5);
 quiver(Grad_X,Grad_Y);
 zoom(2);
 title('Gradien in X-Y-Direction');
@@ -37,7 +42,7 @@ title('Gradien in X-Y-Direction');
 %% Display magnitude values of gradient
 % Attention, Entries of M can be greater than 1
 M=sqrt(Grad_X.*Grad_X+Grad_Y.*Grad_Y); % Magnitude
-figure(5);
+figure(6);
 imshow(M,[])
 colormap(copper)
 colorbar
